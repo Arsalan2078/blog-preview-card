6faed676361587156f66b59152463fe7d4433433
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2024-10-26 00:29:23 -0400

    typography done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App () {
   return (
     <>
-      App
+      <p className="text-preset-1">Text Preset 1</p>
+      <p className="text-preset-2">Text Preset 2</p>
+      <p className="text-preset-3">Text Preset 3</p>
+      <p className="text-preset-3-bold">Text Preset 3 Bold</p>
     </>
   )
 }
